d61735961fab794551f706fd9e0ef5ae9a7fb38f
SANG LE <sqle@ualberta.ca>
2021-10-09 02:54:29 -0600

    format js file

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function validateEmail() {
-    const mail = document.getElementById("email").value
-    const pattern = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+	const mail = document.getElementById('email').value;
+	const pattern = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
 
-    if (mail.length == 0) {
-        document.getElementById("warningMess").innerHTML = "Oops! Please add your email"
-    } else if (!mail.match(pattern)) {
-        document.getElementById("warningMess").innerHTML = "Oops! Please check your email"
-    } else {
-        document.getElementById("warningMess").innerHTML = "Thank you for subscribing!"
-        document.getElementById("warningMess").style.color = "green"
-    }
+	if (mail.length == 0) {
+		document.getElementById('warningMess').innerHTML =
+			'Oops! Please add your email';
+	} else if (!mail.match(pattern)) {
+		document.getElementById('warningMess').innerHTML =
+			'Oops! Please check your email';
+	} else {
+		document.getElementById('warningMess').innerHTML =
+			'Thank you for subscribing!';
+		document.getElementById('warningMess').style.color = 'green';
+	}
 }
