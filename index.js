430318a0da48974d8cc306f08f94c1070b1a936f
Sang Le <57263682+sqle157@users.noreply.github.com>
2022-03-28 00:59:19 -0600

    clean up code

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
+const button = document.querySelector('.btn');
+const warningMess = document.querySelector('#warningMess');
+
+button.addEventListener('click', (e) => {
+	validateEmail();
+
+	// Delete the message after 3 seconds
+	setTimeout(() => {
+		warningMess.textContent = '';
+	}, 3000);
+
+	e.preventDefault();
+});
+
 function validateEmail() {
-	const mail = document.getElementById('email').value;
+	const mail = document.querySelector('.email__input').value;
 	const pattern = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
 
-	if (mail.length == 0) {
-		document.getElementById('warningMess').innerHTML =
-			'Oops! Please add your email';
+	if (mail.length === 0) {
+		warningMess.textContent = 'Oops! Please add your email';
 	} else if (!mail.match(pattern)) {
-		document.getElementById('warningMess').innerHTML =
-			'Oops! Please check your email';
+		warningMess.textContent = 'Oops! Please check your email';
 	} else {
-		document.getElementById('warningMess').innerHTML =
-			'Thank you for subscribing!';
-		document.getElementById('warningMess').style.color = 'green';
+		warningMess.textContent = 'Thank you for subscribing!';
+		warningMess.style.color = 'green';
 	}
+
+	// Clear input fields
+	document.querySelector('.email__input').value = '';
 }
